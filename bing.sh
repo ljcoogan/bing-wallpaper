ef51ea937957000247ebc286c922357cc3783cb7
Liam Coogan <157042199+ljcoogan@users.noreply.github.com>
2024-04-30 22:54:40 +0100

    Substitute wget for curl, notify user if script fails

--- bing.sh
+++ bing.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-# Last modified by Liam Coogan, 2024-01-17
+# Last modified by Liam Coogan, 2024-04-30
 
 # This script will download today's Bing wallpaper to the specified directory,
 # and set it as the wallpaper on the GNOME desktop environment.
@@ -9,27 +9,26 @@
 # place this and 'bing.desktop' in '~/.config/autostart'
 
 # User whose wallpaper will be set
-USER="liam"
+USER=$(whoami)
+
 # Directory wallpaper will be saved to
 DIR="/home/$USER/Pictures/Bing"
-
-
 
 # Allow time for computer to connect to the internet
 sleep 10
 
-# Download XML
-XML=$(curl -s "https://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1")
+# Download the XML metadata for today's image
+XML=$(wget -q -O - "https://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1")
 
 # Ensure XML has downloaded successfully before continuing
 if [[ -n "$XML" ]]
 then
-	# Get image URL
-	IMG="$(echo "$XML" | grep -oP "(?<=<urlBase>).*?(?=</urlBase>)")"
+	# Get URL of image for downloading
+	IMG=$(echo "$XML" | grep -oP "(?<=<urlBase>).*?(?=</urlBase>)")
 	URL="https://bing.com${IMG}_1920x1080.jpg"
 	
 	# File name will be date, formatted YYYYMMDD
-	DATE="$(echo "$XML" | grep -oP "(?<=<startdate>).*?(?=</startdate>)")"
+	DATE=$(echo "$XML" | grep -oP "(?<=<startdate>).*?(?=</startdate>)")
 	FILE="${DIR}/${DATE}"
 	
 	# Ensure file has not already been downloaded before continuing
@@ -37,13 +36,19 @@ then
 	then
 		# Save file to specified location
 		mkdir -p "$DIR"
-		curl -s -o "$FILE" "$URL"
+		wget -q -O "$FILE" "$URL"
 		
 		# Ensure file has downloaded successfully before continuing
 		if [[ -f "$FILE" ]]
 		then
 			# Set GNOME desktop wallpaper for current user
 			gsettings set org.gnome.desktop.background picture-uri "file://${FILE}"
+		else
+			# Notify user file failed to download
+			notify-send "Bing Wallpaper: Failed to download wallpaper."
 		fi
 	fi
+else
+	# Notify user XML failed to download.
+	notify-send "Bing Wallpaper: Failed to download wallpaper."
 fi
